6d43fa92b88c4dcb78e6312970f291fddb154102
krehicbenjamin <krehicbenjamin@gmail.com>
2021-03-25 20:47:32 +0100

    adding ex06

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -5,9 +5,11 @@ let rockStar = new Map([
   ["singer", "Mick Jagger"],
 ]);
 
-let newRock = new Map([...rockStar].filter(([key]) => {
+let newRock = new Map(
+  [...rockStar].filter(([key]) => {
     return key[0] > "a";
-}));
+  })
+);
 
 console.log(newRock);
 module.exports = { rockStar, newRock };
